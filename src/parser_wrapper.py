7b3668e7910dc5dd7e6f50136f292e09958b96f0
Arne Neumann <nlpbox.programming@arne.cl>
2021-01-20 20:49:44 +0100

    cleaned up parser wrapper

--- src/parser_wrapper.py
+++ src/parser_wrapper.py
@@ -22,59 +22,70 @@ def parse_args():
     return parser.parse_args()
 
 
-def create_doc_from_plaintext_file(text_file, annotate_func):
-    with open(text_file, 'r') as fin:
-        input_text = fin.read()
-        doc_tokens = []
-        
-        sentences = annotate_func(input_text)['sentences']
-        for sidx, sent in enumerate(sentences):
-            sent_tokens = []
-            for t in sent['tokens']:
-                token = Token()
-                token.tidx, token.word, token.lemma, token.pos = t['index'], t['word'], t['lemma'], t['pos']
+def create_doc_from_plaintext(input_text, annotate_func):
+    doc_tokens = []
+    
+    sentences = annotate_func(input_text)['sentences']
+    for sidx, sent in enumerate(sentences):
+        sent_tokens = []
+        for t in sent['tokens']:
+            token = Token()
+            token.tidx, token.word, token.lemma, token.pos = t['index'], t['word'], t['lemma'], t['pos']
 
-                # Our input is not annotated with paragraph/sentence/EDU boundaries,
-                # so the paragraph index (pidx) is always 1 and the
-                # EDU index (edudix) always equals the sentence number (sidx + 1).
-                #
-                # Don't ask me why sidx starts counting at 0, but pidx and eduidx start at 1.
-                token.pidx = 1
-                token.sidx = sidx
-                token.eduidx = sidx + 1
-                sent_tokens.append(token)
-            for dep in sent['basicDependencies']:
-                dependent_token = sent_tokens[dep['dependent']-1]
-                dependent_token.hidx = dep['governor']
-                dependent_token.dep_label = dep['dep']
-            doc_tokens += sent_tokens
+            # Our input is not annotated with paragraph/sentence/EDU boundaries,
+            # so the paragraph index (pidx) is always 1 and the
+            # EDU index (edudix) always equals the sentence number (sidx + 1).
+            #
+            # Don't ask me why sidx starts counting at 0, but pidx and eduidx start at 1.
+            token.pidx = 1
+            token.sidx = sidx
+            token.eduidx = sidx + 1
+            sent_tokens.append(token)
+        for dep in sent['basicDependencies']:
+            dependent_token = sent_tokens[dep['dependent']-1]
+            dependent_token.hidx = dep['governor']
+            dependent_token.dep_label = dep['dep']
+        doc_tokens += sent_tokens
 
     doc = Doc()
     doc.init_from_tokens(doc_tokens)
     return doc
 
 
+def parse_text(text, rst_parser, annotate_func, brown_clusters):
+    """Takes an EDU-segmented text (NeuralEDUSeg format) and returns an RST tree
+    (StageDP format)."""
+    doc = create_doc_from_plaintext(text, annotate_func=annotate_func)
 
-def main():
-    args = parse_args()
-    parser = RstParser()
-    parser.load('../data/model')
+    pred_rst = rst_parser.sr_parse(doc, brown_clusters)
+    tree_str = pred_rst.get_parse()
+    return Tree.fromstring(tree_str).pformat(margin=150) + '\n'
+
+
+def load_parser():
+    rst_parser = RstParser()
+    rst_parser.load('../data/model')
     with gzip.open('../data/resources/bc3200.pickle.gz') as fin:
         print('Load Brown clusters for creating features ...')
         brown_clusters = pickle.load(fin)
     core_nlp = StanfordCoreNLP('http://localhost:9000')
 
-    annotate_plaintext = lambda x: core_nlp.annotate(x, properties={
+    annotation_func = lambda x: core_nlp.annotate(x, properties={
         'annotators': 'tokenize,ssplit,pos,lemma,parse,depparse',
         'outputFormat': 'json'
     })
+    return rst_parser, core_nlp, annotation_func, brown_clusters  
 
-    doc = create_doc_from_plaintext_file(args.input_file, annotate_func=annotate_plaintext)
+
+def main():
+    rst_parser, core_nlp, annotation_func, brown_clusters = load_parser()
     
-    pred_rst = parser.sr_parse(doc, brown_clusters)
-    tree_str = pred_rst.get_parse()
-    pprint_tree_str = Tree.fromstring(tree_str).pformat(margin=150) + '\n'
+    args = parse_args()
+    with open(args.input_file, 'r') as input_file:
+        input_text = input_file.read()
     
+    pprint_tree_str = parse_text(input_text, rst_parser=rst_parser, annotate_func=annotation_func, brown_clusters=brown_clusters)
+
     if isinstance(args.output_file, str):
         with open(args.output_file, 'w') as fout:
             fout.write(pprint_tree_str)
